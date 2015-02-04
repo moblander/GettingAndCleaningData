82b17b7e2f4d1c6d44951d1b1a3a7afe7dd09cab
Michael Oblander <moblander@telogical.com>
2015-02-03 22:30:01 -0600

    Updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,55 +1,85 @@
-run_analysis <- function(directory) {
+run_analysis <- function() {
      
-     #directory <- 'C:/Users/moblander/Desktop/Coursera/2 - Getting and Cleaning Data/Course Project/Dataset'
-     activity.labels.file <- paste(directory, 'activity_labels.txt', sep="/");
-     feature.labels.file <- paste(directory, 'features.txt', sep="/");
-     test.file <- paste(directory, 'test/X_test.txt', sep="/");
-     test.subject.file <- paste(directory, 'test/subject_test.txt', sep="/");
-     test.activity.file <- paste(directory, 'test/y_test.txt', sep="/");
-     train.file <- paste(directory, 'train/X_train.txt', sep="/");
-     train.subject.file <- paste(directory, 'train/subject_train.txt', sep="/");
-     train.activity.file <- paste(directory, 'train/y_train.txt', sep="/");
+     # 1. Merge the training and test sets to create one data set:
      
-     test <- read.table(test.file);
-     train <- read.table(train.file);
+          train <- read.table("train/X_train.txt")
+          test <- read.table("test/X_test.txt")
+          
+          data <- rbind(train, test)
      
-     feature.labels <- read.table(feature.labels.file, stringsAsFactors=F)[,2];
-     colnames(test) <- feature.labels;
-     colnames(train) <- feature.labels;
+     # 2. Extract only the measurements on mean and standard deviation:
      
-     feature.labels.mean.std <- c(grep("mean\\(\\)",feature.labels), grep("std\\(\\)",feature.labels));
-     feature.labels.mean.std <- feature.labels.mean.std[order(feature.labels.mean.std)];
+          features <- read.table("features.txt", stringsAsFactors=F)[,2]
+          
+          # Create a vector of indices corresponding to features that contain "mean()" or "std()"
+          features.indices_to_keep <- grep("mean\\(\\)|std\\(\\)", features)
      
-     test <- subset(test, select=feature.labels.mean.std);
-     train <- subset(train, select=feature.labels.mean.std);
+          # Keep only the features that contain "mean()" or "std()"
+          features <- features[features.indices_to_keep]
      
-     test.subject <- read.table(test.subject.file, stringsAsFactors=F)[,1];
-     train.subject <- read.table(train.subject.file, stringsAsFactors=F)[,1];
+          # Remove parenthesis (cosmetic preference)
+          features <- gsub("\\(|\\)", "", features)
+          
+          # Keep only the data measurements on mean and standard deviation
+          data <- subset(data, select=features.indices_to_keep)
      
-     activity.labels <- read.table(activity.labels.file, stringsAsFactors=F);
+     # 3. Use descriptive activity names to name the activities in the data set:
      
-     test.activity <- read.table(test.activity.file, stringsAsFactors=F)[,1];
-     train.activity <- read.table(train.activity.file, stringsAsFactors=F)[,1];
+          activity.labels <- read.table("activity_labels.txt", stringsAsFactors=F)
+          train.activity <- read.table("train/y_train.txt", stringsAsFactors=F)[,1]
+          test.activity <- read.table("test/y_test.txt", stringsAsFactors=F)[,1]
+          
+          # Merge the training.activity and test.activity vectors
+          activity <- append(train.activity, test.activity)
+          
+          # Replace the activity ids with the labels
+          activity <- activity.labels[match(activity, activity.labels[[1]]),2]
      
-     # Replace the activity ids with the labels:
-     test.activity <- activity.labels[match(test.activity, activity.labels[[1]]),2];
-     train.activity <- activity.labels[match(train.activity, activity.labels[[1]]),2];
+     # 4. Appropriately label the data set with descriptive variable names:
      
-     test <- cbind(test.subject, test.activity, test);
-     colnames(test)[1] <- "subject";
-     colnames(test)[2] <- "activity";
+          colnames(data) <- features
      
-     train <- cbind(train.subject, train.activity, train);
-     colnames(train)[1] <- "subject";
-     colnames(train)[2] <- "activity";
+     # 5. Create a data set with the average of each variable for each activity and each subject:
      
-     data <- rbind(test, train);
+          train.subject <- read.table("train/subject_train.txt", stringsAsFactors=F)[,1]
+          test.subject <- read.table("test/subject_test.txt", stringsAsFactors=F)[,1]
+          
+          # Merge the training.subject and test.subject vectors
+          subject <- append(train.subject, test.subject)
      
-     data.by.subject <- split(data, data$subject);
-     data.by.subject <- lapply(data.by.subject, function(x){subset(x, select=c(2:68))});
-     data.by.subject.by.activity <- lapply(data.by.subject, function(x){split(x, x$activity)});
-     data.by.subject.by.activity <- lapply(data.by.subject.by.activity, function(x){lapply(x, function(y){subset(y, select=c(2:67))})});
-     data.by.subject.by.activity <- lapply(data.by.subject.by.activity, function(x){lapply(x, colMeans)});
+          data <- cbind(subject, activity, data, stringsAsFactors=F)
      
-     write.table(data.by.subject.by.activity, "averages_by_activity_for_each_subject.txt", row.names=F);
+          # Split the data into a list of 30 data frames (one for each subject)
+          data <- split(data, data$subject)
+          
+          # Split each of the 30 data frames into a list of 6 data frames (one for each activity)
+          data <- lapply(data, function(x){split(x, x$activity)})
+          
+          numSubjects <- length(unique(subject))
+          numActivities <- length(unique(activity))
+          
+          # Create an empty data frame with descriptive column names
+          column_names <- c("subject", "activity", features)
+          averages <- data.frame(matrix(nrow=0, ncol=length(column_names)))
+          colnames(averages) <- column_names
+          
+          # Populate the data frame with the average of each variable for each activity and each subject
+          row = 1
+          for (i in 1:numSubjects) {
+               
+               for (j in 1:numActivities) {
+                    
+                    observations <- data[[i]][[j]]
+                    numObservations <- length(observations)
+                    
+                    averages[row, "subject"] <- observations$subject[1]
+                    averages[row, "activity"] <- observations$activity[1]
+                    averages[row, 3:numObservations] <- colMeans(observations[, 3:numObservations])
+                    
+                    row = row + 1
+               }
+          }
+     
+          # Write the data set to a text file
+          write.table(averages, "averages_by_subject_and_activity.txt", row.names=F)
 }
